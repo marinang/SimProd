b16778de6cda9cd1847fc7c813dbb255599ea4a8
Matthieu Marinangeli <matthieu.marinangeli@cern.ch>
2019-12-03 11:43:35 +0100

    add simprod/base__init__.py

---
+++ simprod/base__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os, getpass
+
+modulepath = None
+os.environ["SIMPRODPATH"] = modulepath
+
+simoutput = None
+if simoutput is None :
+    simoutput = os.getenv("HOME")+"/SimulationJobs"
+os.system("mkdir -p "+simoutput)
+os.environ["SIMOUTPUT"] = simoutput
+
+#_log_simoutput_ = None
+#_if log_simoutput is None :
+#_	user = getpass.getuser()
+#_	log_simoutput = "/afs/cern.ch/work/{0}/{1}".format( user[0], user )
+#_os.system("mkdir -p "+log_simoutput)
+#_os.environ["LOG_SIMOUTPUT"] = log_simoutput
+
+from .simjob import *
+
