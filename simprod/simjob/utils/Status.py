086e5ebbf97d297982074e3ac071538fcf0fbbaf
Matthieu Marinangeli <matthieu.marinangeli@cern.ch>
2020-07-10 14:38:11 +0200

    remove isvalid in Status

--- simprod/simjob/utils/Status.py
+++ simprod/simjob/utils/Status.py
@@ -24,43 +24,14 @@ class Status(object):
     def __init__(self, status, output, in_init=False):
 
         self.in_init = in_init
-        self.status = "new"
-
-        if os.path.isfile(output):
-            if os.path.getsize(output) >= 700000:
+        
+        if status in ["new", "submitted", "running", "failed"]:
+            self.status = status
+        else:
+            if os.path.isfile(output) and os.path.getsize(output) >= 700000:
                 self.status = "completed"
             else:
                 self.status = "failed"
-        else:
-            if status in ["submitted", "running", "failed"]:
-                self.status = status
-
-        self.creation_time = datetime.datetime.now()
-
-    @property
-    def isvalid(self):
-
-        if self.in_init:
-            return False
-
-        if self.status == "new":
-            delta = TIME_NEW
-        elif self.status == "running":
-            delta = TIME_RUNNING
-        elif self.status == "failed":
-            delta = TIME_FAILED
-        elif self.status == "submitted":
-            delta = TIME_SUBMITTED
-        else:
-            delta = 2
-
-        now = datetime.datetime.now()
-        elapsedTime = now - self.creation_time
-        minutes = divmod(elapsedTime.total_seconds(), 60)[0]
-
-        valid = minutes < delta
-
-        return valid
 
     def __eq__(self, other):
 
